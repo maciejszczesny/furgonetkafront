f8cda93c4a1f85c50be1a04557fe41e10ed0c23a
Maciek-Szczesny <m.szcze15@gmail.com>
2021-05-06 00:49:39 +0200

    Added saving data to mysql; the display of data on the website has not been completed

---
+++ src/app/create-package/Package.ts
@@ -0,0 +1,12 @@
+
+
+export class Package {
+  firstName: string;
+  lastName: string;
+
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
--- src/app/create-package/create-package.component.ts
+++ src/app/create-package/create-package.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import {all} from "codelyzer/util/function";
+import {Package} from "./Package";
 
 @Component({
   selector: 'app-create-package',
@@ -9,6 +10,7 @@ import {all} from "codelyzer/util/function";
   styleUrls: ['./create-package.component.css']
 })
 export class CreatePackageComponent implements OnInit {
+  packages: [];
   /*  profileForm = new FormGroup({
       firstName : new FormControl(''),
       lastName  : new FormControl(''),
@@ -22,11 +24,11 @@ export class CreatePackageComponent implements OnInit {
   profileForm = this.fb.group(
     {
       firstName: [''],
-      lastName: [''],
-      address: [''],
-      country: [''],
-      startPoint: [''],
-      endPoint: [''],
+      lastName: ['']
+      // address: [''],
+      // country: [''],
+      // startPoint: [''],
+      // endPoint: [''],
 });
 
   constructor(private http: HttpClient, private fb: FormBuilder) { }
@@ -35,16 +37,30 @@ export class CreatePackageComponent implements OnInit {
   }
 
   createPackage(): void {
-    this.http.get<any>('http://localhost:8080/packages').subscribe();
+    // const package = new Package(this.profileForm)
+    // this.http.get<any>('http://localhost:8080/packages').subscribe();
+
     console.log('Sending package');
   }
 
   onSubmit() {
     console.warn(this.profileForm.value);
+    this.http.post<any>('http://localhost:8080/packages', this.profileForm.value).subscribe();
+    // console.log(this.profileForm['firstName'].value);
+    // console.log(this.profileForm['lastName'].value);
   }
 
-  get dataPackage(): FormArray {
-    return this.profileForm.get("COS TAKIEGO") as FormArray
-    this.http.get
+  getAllPackages() {
+    this.http.get<any>('http://localhost:8080/packages').subscribe((data) => {
+      this.packages = data;
+      console.log(data);
+    });
+    // console.log(this.profileForm['firstName'].value);
+    // console.log(this.profileForm['lastName'].value);
   }
+
+  // get dataPackage(): FormArray {
+  //   return this.profileForm.get("COS TAKIEGO") as FormArray
+  //   this.http.get
+  // }
 }
